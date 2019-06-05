91de95b7baccd7ac5cadaa73153f7bf2af54dacb
Dan Hughes <danhughes@monzo.com>
2019-06-05 16:15:47 +0100

    Ship to staging by default

--- config/monzo.zsh
+++ config/monzo.zsh
@@ -1,6 +1,7 @@
 [ -f $HOME/src/github.com/monzo/starter-pack/zshrc ] && source $HOME/src/github.com/monzo/starter-pack/zshrc
 
 alias shipper="caffeinate -dis shipper"
-alias ship="shipper deploy"
+alias ship="shipper deploy --s101"
+alias shipp="shipper deploy --prod"
 alias £="£ --prompt.emoji=false"
 alias proto="../bin/generate_protobufs"
