57148fec737ce3dcd3bd14553f2e7b03fa3a6b09
Dan Hughes <danhughes@monzo.com>
2019-11-26 16:27:35 +0000

    Fix prompt locking git

--- config/exports.zsh
+++ config/exports.zsh
@@ -1,3 +1,8 @@
 export LESSOPEN='|pygmentize %s'
+
+# Open VSCode for editing config etc.
 export VISUAL_EDITOR="code --wait"
 export EDITOR="code --wait"
+
+# Don't fetch git remote status in zsh prompt.
+export PURE_GIT_PULL=0
