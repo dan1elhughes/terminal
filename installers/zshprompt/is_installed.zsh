97176d0b70aa69af41a22281da6763736a227bde
Dan Hughes <danhughes@monzo.com>
2020-04-21 21:29:09 +0100

    Fix theme installation detection

--- installers/zshprompt/is_installed.zsh
+++ installers/zshprompt/is_installed.zsh
@@ -1,4 +1,4 @@
-if [ -e "$HOME/.oh-my-zsh/custom/themes/dan1elhughes-terminal.zsh-theme" ]; then
+if [ -e "$HOME/.oh-my-zsh/themes/dan1elhughes-terminal.zsh-theme" ]; then
     exit 0
 else
     exit 1
