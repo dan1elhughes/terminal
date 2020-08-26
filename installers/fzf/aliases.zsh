ade72733b1c33f558dd27527ca0eaa6b27b47d87
Dan Hughes <danhughes@monzo.com>
2020-08-26 12:16:18 +0100

    Add WIP fz function

--- installers/fzf/aliases.zsh
+++ installers/fzf/aliases.zsh
@@ -1,3 +1,8 @@
 function fedit() {
     eval $EDITOR "$(rg --files | fzf --preview 'bat --style numbers,changes --color=always {}')"
 }
+
+# TODO: z not found
+# function fz() {
+#     z | awk '{print $2}' | fzf --preview 'ls {}'
+# }
