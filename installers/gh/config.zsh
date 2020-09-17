525b79e28d94bea6dd25201ca257390947f6b6c4
Dan Hughes <danhughes@monzo.com>
2020-09-17 20:10:21 +0100

    Replace dev build of gh with prod gh

--- installers/gh/config.zsh
+++ installers/gh/config.zsh
@@ -1,10 +1,6 @@
-# ⚠️ When this PR is merged, replace ghd with gh.
-# ghd is the locally built version from the PR.
-# https://github.com/cli/cli/pull/1580
-
 function copypr() {
     echo "⏳ Reading pull request $1..."
-    content=$(ghd pr view $1)
+    content=$(gh pr view $1)
 
     title=$(echo $content | grep "title:" | cut -d$'\t' -f2)
     url=$(echo $content | grep "url:" | cut -d$'\t' -f2)
