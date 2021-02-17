aa8435147a6e7e89733ad959426bfdc78e088807
Dan Hughes <danhughes@monzo.com>
2021-02-17 15:38:05 +0000

    Add stripnewlines fn

--- installers/monzo/aliases.zsh
+++ installers/monzo/aliases.zsh
@@ -85,3 +85,12 @@ gen() {
 
     echo "\nLogs: $LOGFILE"
 }
+
+# Pipe into this function to strip newlines. Useful for editing JSON in a text
+# editor before using it in the CLI, for example:
+# pbpaste | stripnewlines | pbcopy
+stripnewlines() {
+    while read data; do
+        echo $data | tr -d '\n' | tr -s ' '
+    done
+}
