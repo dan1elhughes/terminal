9847b5c777dd8e82dcbe3a42dd50e38a5c5eba1e
Dan Hughes <danhughes@monzo.com>
2022-04-07 12:00:46 +0100

    Better copypr fn

--- installers/gh/aliases.zsh
+++ installers/gh/aliases.zsh
@@ -1,11 +1,13 @@
 function copypr() {
     echo "⏳ Reading pull request $1..."
-    content=$(gh pr view $1)
+    content=$(gh pr view $1 --json additions,deletions,title,url)
 
-    title=$(echo $content | grep "title:" | cut -d$'\t' -f2)
-    url=$(echo $content | grep "url:" | cut -d$'\t' -f2)
+    title=$(echo $content | jq '.title')
+    url=$(echo $content | jq '.url')
+    additions=$(echo $content | jq '.additions')
+    deletions=$(echo $content | jq '.deletions')
 
-    echo -e ":github-green: $title\n:pr-arrow-darkmode: $url" | pbcopy
+    echo -e ":github-green: $title (+$additions, -$deletions)\n:pr-arrow-darkmode: $url" | pbcopy
 
     echo "✅ Pull request copied to clipboard in Slack format."
 }
